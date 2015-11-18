50334352179ae27dde49db4f3e0402d419513803
leunge03 <leunge@hanyang.ac.kr>
2015-11-18 19:07:08 +0900

    first commit

--- linux/shell/move.sh
+++
@@ -1,8 +0,0 @@
-/home/leunge/down/libcoap-4.1.1/examples/./coap-client -e $2 -m put coap://$1/go&
-
-sleep 0.3
-pkill coap
-
-sleep 3
-
-
--- linux/shell/test.sh
+++
@@ -1,3 +0,0 @@
-/var/www/shell/park $1
-
-/var/www/shell/go.sh 0
